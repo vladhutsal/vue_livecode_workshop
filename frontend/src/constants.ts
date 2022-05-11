709aeea313736869a6a01f797daec7e4feca0804
Vlad <vl.hutsal@gmail.com>
2022-05-11 19:44:37 +0300

    Update actions, req.txt and back url

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,3 +1,3 @@
-export const BACK_URL = 'http://localhost:8000/api';
+export const BACK_URL = 'https://ec2-34-207-220-81.compute-1.amazonaws.com:8000/api';
 export const ERROR = true;
 export const SUCCESS = false;
